f742ae01325d9c4ed8b7345325237ff455085a59
srwzz <srwzz@nate.com>
2020-09-25 11:36:19 +0900

    1st commit.

---
+++ src/main/java/mall/DeliveryCancelled.java
@@ -0,0 +1,30 @@
+package mall;
+
+public class DeliveryCancelled extends AbstractEvent {
+
+    private Long id;
+    private String status;
+    private Long OrderId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Long getOrderId() {
+        return OrderId;
+    }
+
+    public void setOrderId(Long OrderId) {
+        this.OrderId = OrderId;
+    }
+}
